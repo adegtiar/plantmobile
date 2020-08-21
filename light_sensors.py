2d08561c8c2d816750b82ca21346bf055f20cad3
Alex Degtiar <alexdegtiar@gmail.com>
2020-08-21 10:10:27 -0700

    Add car vs base sensors and abstractions

--- light_sensors.py
+++ light_sensors.py
@@ -9,30 +9,36 @@ from collections import namedtuple
 from datetime import datetime
 from numpy import mean
 
-CLOSER_SENSOR = 7
-FURTHER_SENSOR = 2
-
-# Create I2C bus as normal
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# Create the TCA9548A object and give it the I2C bus
-# This is the i2c multiplexer used for the light sensors with duplicate addresses.
-tca = adafruit_tca9548a.TCA9548A(i2c)
-
-# For each sensor, create it using the TCA9548A channel instead of the I2C object.
-outer_tsl = adafruit_tsl2561.TSL2561(tca[FURTHER_SENSOR])
-inner_tsl = adafruit_tsl2561.TSL2561(tca[CLOSER_SENSOR])
 
 # A reading of light sensor data.
-LightData = namedtuple('LightData', ['outer', 'inner', 'avg', 'diff', 'diff_percent', 'timestamp'])
+SensorReading = namedtuple('SensorReading', ['outer', 'inner', 'avg', 'diff', 'diff_percent', 'timestamp'])
 
 
-# Get a tuple of the current luminosity reading.
-def read_light_data():
-    outer = outer_tsl.infrared
-    inner = inner_tsl.infrared
-    timestamp = datetime.now()
-    avg = int(mean((outer, inner)))
-    diff = outer - inner
-    diff_percent = int(diff/avg * 100) if avg else 0
-    return LightData(outer, inner, avg, diff, diff_percent, timestamp)
+# Read lux data from a pair of sensors connected to multiplexer.
+class LightSensorReader(object):
+    # This is the i2c multiplexer used for the light sensors (to deal with address conflict).
+    _mux = None
+
+    def __init__(self, outer_pin, inner_pin):
+        # For each sensor, create it using the TCA9548A channel acting as an I2C object.
+        self._outer_tsl = adafruit_tsl2561.TSL2561(LightSensorReader.get_mux()[outer_pin])
+        self._inner_tsl = adafruit_tsl2561.TSL2561(LightSensorReader.get_mux()[inner_pin])
+
+    @classmethod
+    def get_mux(cls):
+        if cls._mux is None:
+            # Create I2C bus as normal.
+            i2c = busio.I2C(board.SCL, board.SDA)
+            # Create the TCA9548A object and give it the I2C bus.
+            cls._mux = adafruit_tca9548a.TCA9548A(i2c)
+        return cls._mux
+
+    # Get a tuple of the current luminosity reading.
+    def read(self):
+        outer = self._outer_tsl.infrared
+        inner = self._inner_tsl.infrared
+        timestamp = datetime.now()
+        avg = int(mean((outer, inner)))
+        diff = outer - inner
+        diff_percent = int(diff/avg * 100) if avg else 0
+        return SensorReading(outer, inner, avg, diff, diff_percent, timestamp)
--- main.py
+++ main.py
@@ -2,23 +2,31 @@
 
 import time
 
-from light_sensors import read_light_data
+from collections import namedtuple
+from light_sensors import LightSensorReader
 from logger import LightCsvLogger
 
-LOG_FILENAME = "data/sensor_log.csv"
 
-logger = LightCsvLogger(LOG_FILENAME)
+CAR_SENSORS = LightSensorReader(outer_pin=2, inner_pin=6)
+CAR_LOGGER = LightCsvLogger("data/car_sensor_log.csv")
+
+BASE_SENSORS = LightSensorReader(outer_pin=1, inner_pin=7)
+BASE_LOGGER = LightCsvLogger("data/base_sensor_log.csv")
+
 
 try:
     while True:
-        lux = read_light_data()
-        print("outer sensor:\t", lux.outer)
-        print("inner sensor:\t", lux.inner)
-        print("average     :\t", lux.avg)
-        print("diff        :\t", lux.diff)
-        print("diff percent:\t {}%".format(lux.diff_percent))
+        car_lux = CAR_SENSORS.read()
+        base_lux = BASE_SENSORS.read()
+        print("sensor:\t\tCar\tBase")
+        print("outer:\t\t{}\t{}".format(car_lux.outer, base_lux.outer))
+        print("inner:\t\t{}\t{}".format(car_lux.inner, base_lux.inner))
+        print("average:\t{}\t{}".format(car_lux.avg, base_lux.avg))
+        print("diff:\t\t{}\t{}".format(car_lux.diff, base_lux.diff))
+        print("diff percent:\t{}%\t{}%".format(car_lux.diff_percent, base_lux.diff_percent))
         print()
-        logger.log(lux)
+        CAR_LOGGER.log(car_lux)
+        BASE_LOGGER.log(base_lux)
         time.sleep(.5)
 except KeyboardInterrupt:
     print("Stopped")
