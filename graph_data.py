989c9256001133491c843b534c746a15bb5893fd
Alex Degtiar <alexdegtiar@gmail.com>
2020-08-21 10:10:06 -0700

    Add average to graph

--- graph_data.py
+++ graph_data.py
@@ -20,23 +20,27 @@ def animate(i):
     fh = open(ftemp)
     outer_luxes = []
     inner_luxes = []
+    avg_luxes = []
     timestamps = []
     for line in fh:
         isotime, outer_lux, inner_lux = line.split(',')
         timestamp = datetime.fromisoformat(isotime)
         outer_luxes.append(float(outer_lux))
         inner_luxes.append(float(inner_lux))
+        avg_luxes.append((float(inner_lux) + float(outer_lux)) / 2)
         timestamps.append(timestamp)
 
     if LAST_N_POINTS:
         outer_luxes = outer_luxes[-LAST_N_POINTS:]
         inner_luxes = inner_luxes[-LAST_N_POINTS:]
+        avg_luxes = avg_luxes[-LAST_N_POINTS:]
         timestamps = timestamps[-LAST_N_POINTS:]
 
     ax1.clear()
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
     ax1.plot_date(timestamps, outer_luxes, '-')
     ax1.plot_date(timestamps, inner_luxes, '-')
+    ax1.plot_date(timestamps, avg_luxes, '-')
     ax1.legend(['Outer Luminosity', 'Inner Luminosity'])
     plt.xlabel('Time')
     plt.ylabel('Lux')
