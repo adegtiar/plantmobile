a031db0167a2f65601b3cb20fbb609a052715f25
Alex Degtiar <alexdegtiar@gmail.com>
2020-08-26 20:49:29 -0700

    Add scaling to led graphs

--- led_graphs.py
+++ led_graphs.py
@@ -20,6 +20,7 @@ class LedBarGraphs(object):
         self.max_level = max_level
         self.num_graphs = num_graphs
         self.high_bit = 0x1 << (num_leds + 1)
+        self.levels_per_led = (max_level - min_level) / num_leds
 
         # Prepare the pin channels for output.
         GPIO.setup(data_pin, GPIO.OUT)
@@ -30,11 +31,14 @@ class LedBarGraphs(object):
     def _set_graph_output(self, level):
         if level > self.max_level:
             level = self.max_level
-        assert level <= self.num_leds, "TODO: implement scaling"
         if level < self.min_level:
             level = 0
+
+        # Scale the level range to the led range.
+        led_level = int((level - self.min_level) / self.levels_per_led)
+
         # Keep all leds on up to and not including the level.
-        output_bits = [0]*(self.num_leds - level) + [1]*level
+        output_bits = [0]*(self.num_leds - led_level) + [1]*led_level
         for led_on in output_bits:
             GPIO.output(self.clock_pin, GPIO.LOW)   # Prepare shift register for input.
             GPIO.output(self.data_pin, GPIO.HIGH if led_on else GPIO.LOW) # Led bit sent on data wire.
@@ -55,15 +59,19 @@ if __name__ == '__main__': # Program entrance
     print ('Running test program for led graphs...' )
     setup()
     try:
-        graphs = LedBarGraphs(data_pin=26, latch_pin=19, clock_pin=13)
-        graphs.set_levels(0, 8)
+        max_level=16
+        graphs = LedBarGraphs(
+                data_pin=26, latch_pin=19, clock_pin=13, min_level=1, max_level=max_level)
+        graphs.set_levels(0, max_level)
         time.sleep(.5)
         while True:
-            for i in range(1, 9):
-                graphs.set_levels(i, 8-i)
+            for i in range(1, max_level+1):
+                print("Setting level to ({}, {})".format(i, max_level-i))
+                graphs.set_levels(i, max_level-i)
                 time.sleep(.5)
-            for i in range(1, 9):
-                graphs.set_levels(8-i, i)
+            for i in range(1, max_level+1):
+                print("Setting level to ({}, {})".format(i, max_level-i))
+                graphs.set_levels(max_level-i, i)
                 time.sleep(.5)
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         graphs.set_levels(0, 0)
