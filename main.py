74db237f39264fc50f32e9e1b6a726611c814037
Alex Degtiar <alexdegtiar@gmail.com>
2020-08-26 21:06:45 -0700

    Generalize status printing

--- main.py
+++ main.py
@@ -23,49 +23,43 @@ def destroy():
     GPIO.cleanup()
 
 
-def car_base_loop():
-    while True:
-        car_lux = CAR_SENSORS.read()
-        base_lux = BASE_SENSORS.read()
-        print("sensor:\t\tCar\tBase")
-        print("outer:\t\t{}\t{}".format(car_lux.outer, base_lux.outer))
-        print("inner:\t\t{}\t{}".format(car_lux.inner, base_lux.inner))
-        print("average:\t{}\t{}".format(car_lux.avg, base_lux.avg))
-        print("diff:\t\t{}\t{}".format(car_lux.diff, base_lux.diff))
-        print("diff percent:\t{}%\t{}%".format(car_lux.diff_percent, base_lux.diff_percent))
-        print()
-        CAR_LOGGER.log(car_lux)
-        BASE_LOGGER.log(base_lux)
-        time.sleep(.5)
-
-
-def base_only_loop():
-    while True:
-        base_lux = BASE_SENSORS.read()
-        print("sensor:\t\tCar\tBase")
-        print("outer:\t\tN/A\t{}".format(base_lux.outer))
-        print("inner:\t\tN/A\t{}".format(base_lux.inner))
-        print("average:\tN/A\t{}".format(base_lux.avg))
-        print("diff:\t\tN/A\t{}".format(base_lux.diff))
-        print("diff percent:\tN/A\t{}%".format(base_lux.diff_percent))
-        print()
-        BASE_LOGGER.log(base_lux)
-        time.sleep(.5)
-
-
-def loop():
-    BASE_SENSORS.initialize()
-    try:
-        CAR_SENSORS.initialize()
-    except ValueError: # This might happen if the car is disconnected
-        base_only_loop()
+def print_status(base_lux, car_lux):
+    if car_lux:
+        car_outer, car_inner, car_avg, car_diff, car_diff_percent, _ = car_lux
     else:
-        car_base_loop()
+        car_outer, car_inner, car_avg, car_diff, car_diff_percent = ("N/A",) * 5
+    print("sensor:\t\tCar\tBase")
+    print("outer:\t\t{}\t{}".format(car_outer, base_lux.outer))
+    print("inner:\t\t{}\t{}".format(car_inner, base_lux.inner))
+    print("average:\t{}\t{}".format(car_avg, base_lux.avg))
+    print("diff:\t\t{}\t{}".format(car_diff, base_lux.diff))
+    print("diff percent:\t{}%\t{}%".format(car_diff_percent, base_lux.diff_percent))
+    print()
+
+
+def loop(base_sensors, car_sensors):
+    base_sensors.initialize()
+    try:
+        car_sensors.initialize()
+    except ValueError: # This might happen if the car is disconnected
+        car_sensors = None
+
+    while True:
+        base_lux = base_sensors.read()
+        car_lux = car_sensors.read() if car_sensors else None
+
+        print_status(base_lux, car_lux)
+
+        BASE_LOGGER.log(base_lux)
+        if car_sensors:
+            CAR_LOGGER.log(car_lux)
+
+        time.sleep(.5)
 
 
 if __name__ == '__main__':
     try:
-        loop()
+        loop(BASE_SENSORS, CAR_SENSORS)
     except KeyboardInterrupt:
         destroy()
         print("Stopped")
