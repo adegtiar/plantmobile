01abc1e03958fda6efe87ef237f8c02ad9cbb44f
Alex Degtiar <alexdegtiar@gmail.com>
2020-09-05 17:28:38 -0700

    Add adafruit distance sensor lib

--- ultrasonic_ranging.py
+++ ultrasonic_ranging.py
@@ -8,10 +8,19 @@
 import RPi.GPIO as GPIO
 import time
 
-trigPin = 4
-echoPin = 17
+import adafruit_hcsr04
+import board
+
 MAX_DISTANCE = 100          # define the maximum measuring distance, unit: cm
 timeOut = MAX_DISTANCE*60   # calculate timeout according to the maximum measuring distance
+
+
+#class DistanceSensor(object):
+#
+#    def __init__(self, trig_pin, echo_pin, max_distance=MAX_DISTANCE):
+#        self.trig_pin = trig_pin
+#        self.echo_pin = echo_pin
+#        self.timeout = max_distance*60
 
 def pulseIn(pin,level,timeOut): # obtain pulse time of a pin under timeOut
     t0 = time.time()
@@ -35,22 +44,44 @@ def getSonar():     # get the measurement results of ultrasonic module,with unit
     
 def setup():
     GPIO.setmode(GPIO.BCM)          # use BCM GPIO Numbering
-    GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
-    GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
+    #GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
+    #GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
 
 def loop():
     while(True):
         distance = getSonar() # get distance
         print ("The distance is : %.2f cm"%(distance))
         time.sleep(.1)
-        
+
+def loop_gpiozero(distance_sensor):
+    sensor = DistanceSensor(echo=echoPin, trigger=trigPin, threshold_distance=0.1, partial=True, queue_len=1)
+    while(True):
+        distance = distance_sensor.distance * 100
+        print ("The distance is : %.2f cm"%(distance))
+        time.sleep(.1)
+
+def loop_adafruit():
+    sensor = adafruit_hcsr04.HCSR04(trigger_pin=board.D4, echo_pin=board.D17)
+    while(True):
+        try:
+            distance = sensor.distance
+            print ("The distance is : %.2f cm"%(distance))
+        except RuntimeError:
+            print ("time out waiting for response")
+        time.sleep(.1)
+
+
+trigPin = 4
+echoPin = 17
+
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
     setup()
     try:
-        loop()
+        loop_adafruit()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
-        GPIO.cleanup()         # release GPIO resource
+        #GPIO.cleanup()         # release GPIO resource
+        pass
 
 
     
