958c36b8fb021b2dfb04da6eccd1eb5722a3c998
Alex Degtiar <alexdegtiar@gmail.com>
2020-09-07 11:12:04 -0700

    Fix sonic ranging blips

--- ultrasonic_ranging.py
+++ ultrasonic_ranging.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import itertools
+import logging
 import RPi.GPIO as GPIO
 import time
 
@@ -16,6 +17,7 @@ class DistanceSensor(object):
         self.threshold_cm = threshold_cm
         self.timeout = timeout
         self._sensor = None
+        self._prev_distance = None
 
     def setup(self):
         if self._sensor is None:
@@ -32,9 +34,16 @@ class DistanceSensor(object):
         Returns inf when no response is heard within timeout."""
         self.setup()
         try:
-            return self._sensor.distance
+            self._prev_distance = self._sensor.distance
         except RuntimeError:
-            return float("inf")
+            logging.warn("Failed to read distance. Defaulting to previously read value")
+
+        if self._prev_distance is None:
+            logging.warn("Initializing first value to inf and retrying")
+            self._prev_distance = float("inf")
+            return self.get_distance()
+        else:
+            return self._prev_distance
 
     def is_in_range(self):
         return self.get_distance() < self.threshold_cm
@@ -44,19 +53,19 @@ def loop(sensor):
     out_of_range = False
     for i in itertools.count():
         distance = sensor.get_distance()
-        if out_of_range or i % 10 == 0:
+        if out_of_range or i % 1 == 0:
             print("The distance is : %.2f cm" % (distance))
         out_of_range = distance == float("inf")
         if out_of_range:
             out_of_range = True
             print("sensor is out of range")
-        time.sleep(.01)
-
+        time.sleep(.1)
 
 
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
     sensor = DistanceSensor(trig_pin=4, echo_pin=17, threshold_cm=10)
+
     try:
         loop(sensor)
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
